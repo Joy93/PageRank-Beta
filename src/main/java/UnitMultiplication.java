efc57d121d19b6450b37ab646270a8add8b3fbcc
YEJING ZHOU <zhouyj0118@gmail.com>
2017-08-20 15:18:57 -0500

    PageRank
    An implementation of Page Rank algorithm based on MapReduce and Hadoop

---
+++ src/main/java/UnitMultiplication.java
@@ -0,0 +1,95 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnitMultiplication {
+
+    public static class TransitionMapper extends Mapper<Object, Text, Text, Text> {
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString().trim();
+            String[] fromTo = line.split("\t");
+
+            if(fromTo.length == 1 || fromTo[1].trim().equals("")) {
+                return;
+            }
+            String from = fromTo[0];
+            String[] tos = fromTo[1].split(",");
+            for (String to: tos) {
+                context.write(new Text(from), new Text(to + "=" + (double)1/tos.length));
+            }
+        }
+    }
+
+    public static class PRMapper extends Mapper<Object, Text, Text, Text> {
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String[] pr = value.toString().trim().split("\t");
+            context.write(new Text(pr[0]), new Text(pr[1]));
+        }
+    }
+
+    public static class MultiplicationReducer extends Reducer<Text, Text, Text, Text> {
+
+
+        @Override
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+              List<String> transitionUnits = new ArrayList<String>();
+            double prUnit = 0;
+            double beta = 0.25;
+
+            for(Text value: values) {
+                String curr = value.toString().trim();
+                if(curr.contains("=")) {
+                    transitionUnits.add(curr);
+                }else {
+                    prUnit = Double.parseDouble(curr);
+                }
+            }
+
+            for(String unit: transitionUnits) {
+                String[] strs = unit.split("=");
+                String toPage = strs[0];
+                double prob = Double.parseDouble(strs[1]);
+                //how to tackle dead end and spider traps.
+                String toValue = String.valueOf(prob * (1 - beta) * prUnit + beta * prUnit);
+                context.write(new Text(toPage), new Text(toValue));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(UnitMultiplication.class);
+
+        job.setReducerClass(MultiplicationReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TransitionMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, PRMapper.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.waitForCompletion(true);
+    }
+
+}
